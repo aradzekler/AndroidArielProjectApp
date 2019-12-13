0c2f522fd7fce62b6b186c7f61becb56a0c62192
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2019-12-13 16:04:34 +0200

    added an activity for map display

--- app/src/main/java/com/example/androidarielprojectapp/test/MainActivity.java
+++ app/src/main/java/com/example/androidarielprojectapp/test/MainActivity.java
@@ -6,13 +6,17 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.androidarielprojectapp.R;
 
+/**
+ * our main activity screen
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     Button userRentButton;
@@ -28,12 +32,12 @@ public class MainActivity extends AppCompatActivity {
         superRentButton = (Button)findViewById(R.id.superRentButton);
         logInButton = (Button)findViewById(R.id.logInButton);
 
+
         userRentButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                statusCheck(); // check if location services are on
                 Intent i = new Intent(getApplicationContext(),UserRentActivity.class);
                 startActivity(i);
             }
@@ -45,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                statusCheck();
                 Intent i = new Intent(getApplicationContext(),SuperRentActivity.class);
                 startActivity(i);
             }
@@ -61,36 +64,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
     }
 
 
-    // check if gps is enabled
-    public void statusCheck() {
-        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            buildAlertMessageNoGps();
-
-        }
-    }
-
-    // alert massage for when no location services are present, enables user to turn on location services
-    private void buildAlertMessageNoGps() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, final int id) {
-                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, final int id) {
-                        dialog.cancel();
-                    }
-                });
-        final AlertDialog alert = builder.create();
-        alert.show();
-    }
 
 }
