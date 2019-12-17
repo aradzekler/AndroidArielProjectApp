02bc464c0a8db1e25bd9e07f457c076927f32a39
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2019-12-17 17:29:03 +0200

    added more layout functionality

--- app/src/main/java/com/example/androidarielprojectapp/test/NewRentalActivity.java
+++ app/src/main/java/com/example/androidarielprojectapp/test/NewRentalActivity.java
@@ -1,21 +1,96 @@
 package com.example.androidarielprojectapp.test;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.graphics.Bitmap;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidarielprojectapp.R;
 
+import java.io.IOException;
+
+
 public class NewRentalActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private int PICK_IMAGE_REQUEST = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_rental_activity);
 
-        Intent intent = getIntent();
+        Button selectImage = (Button)findViewById(R.id.add_img_btn);
+        Button registerVe = (Button)findViewById(R.id.reg_btn);
+        final RadioGroup rg = findViewById(R.id.type_radio_group);
+        final RadioButton scooterBtn = (RadioButton)findViewById(R.id.scooter_radio_btn);
+        final RadioButton biBtn = (RadioButton)findViewById(R.id.bicycle_radio_btn);
 
-        String loc = intent.getStringExtra("RENTAL_LOCATION");
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        registerVe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rg.getCheckedRadioButtonId() == -1)
+                {
+                    // no radio buttons are checked
+                }
+                else if(scooterBtn.isChecked())
+                {
+                    // one of the radio buttons is checked
+                }
+                else if (biBtn.isChecked()) {
+
+                }
+            }
+        });
+    }
+
+    public void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = findViewById(R.id.imgView);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
