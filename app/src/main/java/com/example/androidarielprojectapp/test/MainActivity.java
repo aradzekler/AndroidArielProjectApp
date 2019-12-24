a300d07776e29937b5c1bc1c8452ae63e8aa41bf
Naor Dahan <naordahan@hotmail.com>
2019-12-25 00:36:04 +0200

    added logout button + fixed transition between activities

--- app/src/main/java/com/example/androidarielprojectapp/test/MainActivity.java
+++ app/src/main/java/com/example/androidarielprojectapp/test/MainActivity.java
@@ -6,19 +6,19 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.androidarielprojectapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * our main activity screen
  */
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseAuth user;
     Button userRentButton;
     Button superRentButton;
     Button logInButton;
+    Button logOutButton;
     boolean regStatus = true; // if the user is registered it will be turned on. false by default. true for testing.
 
     @Override
@@ -26,11 +26,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
         userRentButton = (Button) findViewById(R.id.userRentButton);
         superRentButton = (Button) findViewById(R.id.superRentButton);
         logInButton = (Button) findViewById(R.id.logInButton);
-
+        logOutButton = (Button) findViewById(R.id.Logout);
 
         userRentButton.setOnClickListener(new View.OnClickListener() {
 
@@ -42,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                    Intent i = new Intent(getApplicationContext(), Login.class);
+                    startActivity(i);
+            }
+        });
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(MainActivity.this, "User logged out successfully!", Toast.LENGTH_LONG).show();
+                FirebaseAuth.getInstance().signOut();
+                finish();
+            }
+        });
 
         superRentButton.setOnClickListener(new View.OnClickListener() {
 
@@ -57,17 +77,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-        logInButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                startActivity(new Intent(getApplicationContext(),Login.class));
-            }
-        });
-
 
     }
 
