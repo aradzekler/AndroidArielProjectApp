8be706801baf718343647be46e1dc60906a4438e
aradzekler <mcspancy@gmail.com>
2019-12-25 17:11:45 +0200

    problem with map still not fixed

--- app/src/main/java/com/example/androidarielprojectapp/test/MapsActivity.java
+++ app/src/main/java/com/example/androidarielprojectapp/test/MapsActivity.java
@@ -10,8 +10,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
-
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
